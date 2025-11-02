d0fe8051be808cfb65260302a82f54bdf37628be
kazaza021 <pooklnw.2546@gmail.com>
2025-11-02 21:59:44 +0700

    First Commit Firebase Nextjs

---
+++ component/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="flex flex-col items-center mt-10">
+        <span className="text-lg  text-grey-700">Created by Satjatum K.</span>
+        <span className="text-lg  text-grey-700">
+          Copyright 2025 Southeast Asia University
+        </span>
+      </div>
+    </>
+  );
+}
